69f85071220c6572c8e97806f2e294bea0e92ecb
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-10 16:33:53 +0200

    style: card looking exactly like spotify's

--- src/shared/Card.tsx
+++ src/shared/Card.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
-            Card
-        </div>
+        <article className="card">
+            <div className="cover">
+                <div className="pb-wrapper">
+                    <button className="play-button">
+                        <i className="fas fa-play play"/>
+                    </button>
+                </div>
+                <img src="https://via.placeholder.com/150" alt="" className="thumbnail"/>
+            </div>
+            <div className="description">
+                <h1 className="pl-name">Global Top 50</h1>
+                <p className="pl-about">Your daily update of the most played tracks right now.</p>
+            </div>
+        </article>
     );
 };
 
