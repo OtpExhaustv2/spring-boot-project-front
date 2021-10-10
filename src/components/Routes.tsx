ea95fefccb2b584867278a9e8515280d75808180
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-10 16:35:00 +0200

    style: enclosed all routes with content class for padding

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -10,25 +10,29 @@ import Liked from "../pages/Liked";
 import Sidebar from "./Sidebar/Sidebar";
 import Navbar from "./Navbar";
 import Footer from "./Footer/Footer";
+import Queue from "../pages/Queue";
 
 const Routes = () => {
     return (
         <Router>
-            <div className="player">
+            <div>
                 <div className="player__body">
                     <Sidebar/>
                     <div className="body">
                         <Navbar/>
-                        <Switch>
-                            <Route path='/' exact component={Home}/>
-                            <Route path='/profile' exact component={Profile}/>
-                            <Route path='/search' exact component={Search}/>
-                            <Route path='/library' exact component={Library}/>
-                            <Route path='/create-playlist' exact component={CreatePlaylist}/>
-                            <Route path='/playlist/:id' component={Playlist}/>
-                            <Route path='/liked-titles' exact component={Liked}/>
-                            <Redirect to='/'/>
-                        </Switch>
+                        <div className="content">
+                            <Switch>
+                                <Route path='/' exact component={Home}/>
+                                <Route path='/profile' exact component={Profile}/>
+                                <Route path='/search' exact component={Search}/>
+                                <Route path='/library' exact component={Library}/>
+                                <Route path='/create-playlist' exact component={CreatePlaylist}/>
+                                <Route path='/playlist/:id' component={Playlist}/>
+                                <Route path='/liked-titles' exact component={Liked}/>
+                                <Route path='/queue' exact component={Queue}/>
+                                <Redirect to='/'/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
                 <Footer/>
