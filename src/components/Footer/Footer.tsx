ffc2fa6204cff35874a7ad276ec3e45ff2fd4403
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-10 16:34:26 +0200

    style: bottom sticky footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {musicsActionCreators, State} from "../../state";
 import {bindActionCreators} from "redux";
@@ -6,29 +6,28 @@ import AudioPlayer from "./AudioPlayer";
 import Music from "../../models/Music";
 
 const Footer = () => {
-    const dispatch = useDispatch();
-    const {like} = bindActionCreators(musicsActionCreators, dispatch);
-    const music = useSelector((state: State) => state.music);
+    //const dispatch = useDispatch();
+    const [isLiked, setIsLiked] = useState(false);
+    // const {like} = bindActionCreators(musicsActionCreators, dispatch);
+    // const music = useSelector((state: State) => state.music);
 
     return (
         <div className="footer">
             <div className="footer__left">
-                <p>Album and song details</p>
+                <div className="song">
+                    <img
+                        src="https://picsum.photos/150/150"
+                        alt=""/>
+                    <div className="song-details">
+                        <h3>My Universe</h3>
+                        <p> Coldplay, BTS</p>
+                    </div>
+                    <div className={`favorite ${isLiked && 'active'}`} onClick={() => setIsLiked(n => !n)}><i
+                        className="far fa-heart"/></div>
+                </div>
             </div>
             <AudioPlayer/>
         </div>
-        // <>
-        //     {Object.keys(music).length !== 0 &&
-        //     <div>
-        //         <img src={music.img} alt=""/>
-        //         <p>{music.title}</p>
-        //         <p>{music.group}</p>
-        //         <p onClick={() => like(music)}>
-        //             {music.liked ? <i className="fas fa-heart"/> : <i className="far fa-heart"/>}
-        //         </p>
-        //     </div>
-        //     }
-        // </>
     );
 };
 
