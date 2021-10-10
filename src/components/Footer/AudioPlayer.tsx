33d9041f40943ba95fc3e170c5e9d74dfd52dfb5
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-10 16:36:43 +0200

    feature: mute the sound & show the current listening playlist by toggling a button

--- src/components/Footer/AudioPlayer.tsx
+++ src/components/Footer/AudioPlayer.tsx
@@ -1,15 +1,15 @@
 import React, {MutableRefObject, useEffect, useRef, useState} from 'react';
-// import ShuffleIcon from '@material-ui/icons/Shuffle';
-// import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
-// import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-// import SkipNextIcon from '@material-ui/icons/SkipNext';
-// import RepeatIcon from '@material-ui/icons/Repeat';
-// import PauseCircleButton from '@material-ui/icons/PauseCircleOutline';
+import {NavLink, useHistory} from 'react-router-dom';
 
 const AudioPlayer = () => {
+        const history = useHistory();
+
         const [isPlaying, setIsPlaying] = useState(false);
         const [shuffle, setShuffle] = useState(false);
         const [loop, setLoop] = useState(false);
+        const [previousVolumeValue, setPreviousVolumeValue] = useState("0.5");
+        const [volumeIcon, setVolumeIcon] = useState("fas fa-volume-down");
+        const [isQueueOpen, setIsQueueOpen] = useState(false);
 
         const [duration, setDuration] = useState(0);
         const [currentTime, setCurrentTime] = useState(0);
@@ -19,23 +19,24 @@ const AudioPlayer = () => {
         const progressVolume = useRef() as MutableRefObject<HTMLInputElement>;
         const animationRef = useRef() as MutableRefObject<FrameRequestCallback>;
 
+        useEffect(() => {
+            progressVolume.current.style.setProperty('--seek-before-width', `${(Number(progressVolume.current.value) * 100)}%`);
+        }, []);
+
+        useEffect(() => {
+            if (!loop && currentTime === duration) {
+                setIsPlaying(false);
+            }
+        }, [currentTime, duration, loop]);
+
         const onLoadedMetadata = () => {
             if (audioPlayer.current) {
                 const seconds = Math.floor(audioPlayer.current.duration);
                 setDuration(seconds);
                 progressBar.current.max = String(seconds);
+                audioPlayer.current.volume = Number(progressVolume.current.value);
             }
         }
-
-        useEffect(() => {
-            progressVolume.current.style.setProperty('--seek-before-width', `${(Number(progressVolume.current.value) * 100)}%`);
-        }, [progressVolume?.current]);
-
-        useEffect(() => {
-            if (!loop && currentTime == duration) {
-                setIsPlaying(false);
-            }
-        }, [currentTime]);
 
         const calculateTime = (secs: number) => {
             const minutes = Math.floor(secs / 60);
@@ -78,12 +79,39 @@ const AudioPlayer = () => {
         const changeVolume = () => {
             audioPlayer.current.volume = Number(progressVolume.current.value);
             progressVolume.current.style.setProperty('--seek-before-width', `${Number(progressVolume.current.value) * 100}%`);
+            if (progressVolume.current.value === "0") {
+                setVolumeIcon("fas fa-volume-mute");
+            } else {
+                setVolumeIcon("fas fa-volume-down");
+            }
+        }
+
+        const mute = () => {
+            const {value} = progressVolume.current;
+            setPreviousVolumeValue(value);
+            if (value === "0") {
+                progressVolume.current.value = previousVolumeValue === "0" ? String(Number(progressVolume.current.max) / 2) : previousVolumeValue;
+                setVolumeIcon("fas fa-volume-down");
+            } else {
+                progressVolume.current.value = "0";
+                setVolumeIcon("fas fa-volume-mute");
+            }
+            changeVolume();
+        }
+
+        const toggleQueue = () => {
+            setIsQueueOpen(n => !n);
+            if (isQueueOpen) {
+                history.goBack();
+            } else {
+                history.push('/queue');
+            }
         }
 
         return (
             <>
                 <div className="footer__center">
-                    <div className="footer__player">
+                    <div className="player">
                         <audio ref={audioPlayer}
                                src={`${process.env.PUBLIC_URL}/songs/acdc-back-in-black-official-video.mp3`}
                                preload="metadata" loop={loop} onLoadedMetadata={onLoadedMetadata}/>
@@ -99,16 +127,20 @@ const AudioPlayer = () => {
                             className="fas fa-undo-alt"/></button>
                     </div>
 
-                    <div className="footer__timer">
+                    <div className="timer">
                         <div className="current-time">{calculateTime(currentTime)}</div>
-                        <div>
-                            <input type="range" className="progress-bar" defaultValue="0" ref={progressBar}
-                                   onChange={changeRange}/>
-                        </div>
+                        <input type="range" className="progress-bar" defaultValue="0" ref={progressBar}
+                               onChange={changeRange}/>
                         <div className="duration">{(!isNaN(duration) && duration) && calculateTime(duration)}</div>
                     </div>
                 </div>
                 <div className="footer__right">
+                    <button onClick={toggleQueue}>
+                        <i className={`fas fa-stream waiting-queue ${isQueueOpen && 'active'}`}/>
+                    </button>
+                    <button onClick={mute}>
+                        <i className={volumeIcon}/>
+                    </button>
                     <input type="range" onChange={changeVolume} ref={progressVolume} min="0" max="1" step="0.01"
                            defaultValue="0.1"/>
                 </div>
@@ -118,16 +150,3 @@ const AudioPlayer = () => {
 ;
 
 export default AudioPlayer;
-
-{/*<ShuffleIcon className={`footer__icon ${shuffle && 'active'}`} onClick={() => setShuffle(n => !n)}/>*/
-}
-{/*<SkipPreviousIcon className="footer__icon"/>*/
-}
-{/*{!isPlaying ? <PlayCircleOutlineIcon className="footer__icon play" onClick={togglePlayPause}/> :*/
-}
-{/*    <PauseCircleButton className="footer__icon" onClick={togglePlayPause}/>}*/
-}
-{/*<SkipNextIcon className="footer__icon"/>*/
-}
-{/*<RepeatIcon className="footer__icon" onClick={() => setLoop(n => !n)}/>*/
-}
