c510771cdc4e58342c726fcee9f49bed54987292
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-11 13:09:52 +0200

    feat: fetching fake data for simulating a lot of cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import Card from "../shared/Card";
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const result = await axios.get('https://jsonplaceholder.typicode.com/todos');
+            setPosts(result.data);
+        }
+        getPosts();
+    }, []);
+
     return (
         <main className="home">
             <h3>Écoutés récemment</h3>
             <div className="cards">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                {posts.map((post: any) => <Card key={post.id}/>)}
             </div>
         </main>
     );
