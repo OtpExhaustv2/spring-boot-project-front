391e5047f95ad441f3653e52d3e19f6f22495b1b
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-11 13:09:14 +0200

    feat: advertisement with redirecting to the website

---
+++ src/shared/Ad.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Ad = () => {
+    return (
+        <div className="advertisement">
+            <a href="https://www.google.com" target="_blank" rel="noreferrer">
+                <img className="ad" src="https://i.ytimg.com/vi/wq7R9zTjBOY/maxresdefault.jpg" alt="basic fit pub"/>
+            </a>
+        </div>
+    );
+};
+
+export default Ad;
