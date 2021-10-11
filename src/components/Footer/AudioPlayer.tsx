f1d7430c84796e9efbd9729bd6db29221a931425
OtpExhaustv2 <sebastien.vanvreckem@gmail.com>
2021-10-11 13:09:33 +0200

    refactor: removed unused import

--- src/components/Footer/AudioPlayer.tsx
+++ src/components/Footer/AudioPlayer.tsx
@@ -1,5 +1,5 @@
 import React, {MutableRefObject, useEffect, useRef, useState} from 'react';
-import {NavLink, useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 const AudioPlayer = () => {
         const history = useHistory();
